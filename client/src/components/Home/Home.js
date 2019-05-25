e637886926f95ef6db5ef46ddd0f67525cd4364e
Marion <marion_ott@me.com>
2019-05-26 00:13:30 +0200

    home --no animation

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,31 +1,50 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import './styles.scss'
-import Loader from '../Loader/Loader'
-import { TimelineLite } from 'gsap'
+import { TimelineLite, Power2 } from 'gsap'
 
 class Home extends React.Component {
     constructor(props) {
         super(props)
         this.title = null
+        this.line = null
+        this.lineAfter = null
         this.scrollIcon = null
+        this.background = null
         this.tl = new TimelineLite({paused: true})
     }
 
     componentDidMount() {
-        this.tl.to(ReactDOM.findDOMNode(this.loader), 1, { opacity:0.6, delay: 3})
-               .to(this.title, 1, {y: -50, opacity: 1},3.5)
-               .to(this.scrollIcon, 1, {y: -10, opacity: 1, delay: 1})
-        this.tl.play()
+        // this.tl.to(this.line, 1, {ease: Power2.easeOut, width: '100%', delay: 1})
+        //        .to(this.lineAfter, 0, {display: 'block'})
+        //        .to(this.line, 0, {display: 'none'})
+        //        .to(this.lineAfter, 1, {ease: Power2.easeOut, width: '0%', delay: 1})
+        //        .to(this.background, 2, {ease: Power2.easeOut, opacity: '1'})
+        // this.tl.play()
     }
+
 
     render() {
         return(
             <div className="Home">
-                <div className="Home--overlay"></div>
-                <Loader ref={el => this.loader = el} />
-                <div className="Home--rings" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}assets/img/olympics-rings.png)` }}></div>
-                <h1 ref={el => this.title = el} className="Home_title">Inside the Olympics<br/><span className="date">(1896 - 2016)</span></h1>
+                <div className="Home_logo">
+                    <img src={`${process.env.PUBLIC_URL}assets/img/rings-olympics.png`} alt=""/>
+                </div>
+                {/* <div className="Home_loader">
+                    <div ref={el => this.line = el} className="Home_loader_line"></div>
+                    <div ref={el => this.lineAfter = el} className="Home_loader_lineAfter"></div>
+                </div> */}
+                <div ref={el => this.background = el}>
+                    <div className="Home_background back" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}assets/img/background_fond.png)` }}></div>
+                    <div className="Home_background front" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}assets/img/background_front.png)` }}></div>
+                </div>
+                <div className="Home_title">
+                    <h1 ref={el => this.title = el}>Les jeux<br/>Olympiques</h1>
+                    <div className="date">
+                        <div></div>
+                        <h3>1896 - 2016</h3>
+                    </div>
+                </div>
                 <div ref={el => this.scrollIcon = el} className="scroller">
                     <div className="mouse">
                         <div className="ball"></div>
