c9de89c589577a4f1a6b401bc09e0c5ed60bd011
Marion <marion_ott@me.com>
2019-05-31 09:03:35 +0200

    remove node_modules from root & add fact.js

---
+++ server/facts.js
@@ -0,0 +1,223 @@
+let facts = [
+    {
+        id: 1,
+        src: "assets/img/fact-1896.jpg",
+        title: "Les Jeux Olympiques de 1896, également nommés Jeux de la première olympiade.",
+        text: "C'est à l'issue d'un congrès organisé en 1894 à Paris par le Français Pierre de Coubertin qu'est créé le Comité International Olympique (CIO) et que la capitale grecque est désignée première ville hôte de l'événement olympique. Ce congrès décide également de l'exclusion des sports professionnels et des femmes au profit de de l'amateurisme et du sexe masculin.", 
+    },
+
+    {
+        id: 2,
+        src: "assets/img/fact-1900.jpg",
+        title:"Les femmes participent pour la première fois aux Jeux Olympiques en 1900.",
+        text: "Selon les chiffres du CIO et de Bill Mallon, 22 femmes sont présentes. Des épreuves féminines de golf et de tennis sont organisées et quelques femmes participent à des épreuves mixtes en voile, en croquet et en équitation. La joueuse de tennis britannique Charlotte Cooper devient la première championne olympique dans une épreuve individuelle."
+    },
+
+    {
+        id: 3,
+        src: "assets/img/fact-1904.jpg",
+        title:"James Sullivan, directeur des Jeux de 1904 décide de lancer le marathon à 14h30 alors que le mercure grimpe à 32 °C et que le taux d'humidité s'élève à 90 %.",
+        text: "En nage avant même d'avoir couru, les 32 athlètes s'alignent fébrilement au départ. Le vainqueur, Thomas Hicks, passe la ligne d'arrivée soutenu par ses suiveurs. Il s'effondre peu après et doit être réanimé."
+    },
+
+    {
+        id: 4,
+        src: "assets/img/fact-1908.jpg",
+        title:"Les Jeux Olympiques de 1908 furent attribués dans un premier temps à la ville de Rome au détriment de Milan et Berlin.",
+        text: "Mais l'éruption du Vésuve en avril 1906 remet en cause l'organisation des Jeux Olympiques à Rome et oblige le gouvernement italien à revenir sur son engagement pour des raisons financières dues aux coûts de reconstruction. À la suite de ce forfait, les villes de Berlin et de Londres posent leur candidature. Le 24 novembre 1906, le CIO désigne finalement Londres ville hôte des Jeux de 1908."
+    },
+
+    {
+        id: 5,
+        src: "assets/img/fact-1912.jpg",
+        title:"Un bond en avant",
+        text: "Modèle d'efficacité, les Jeux de Stockholm sont les premiers à utiliser des systèmes de chronométrage automatique pour les épreuves sur piste, de photo finish et de sonorisation."
+    },
+
+    {
+        id: 6,
+        src: "assets/img/fact-1920.jpg",
+        title:"Les jeux de 1916 prévus à Berlin furent annulés en raison de la Première Guerre mondiale.",
+        text: "Ceux de 1920 furent décernées à Anvers pour rendre hommage aux souffrances infligées à la population belge durant la guerre1. Budapest fut dans un premier temps désignée mais l'empire austro-hongrois étant un allié de l'Allemagne, les jeux lui furent retirés au profit d'Anvers. À ce titre, et contre l'avis de Pierre de Coubertin, l'Allemagne, l'Autriche, la Hongrie, l'Empire ottoman et la Bulgarie furent exclues par le Comité d'Organisation."
+    },
+
+    {
+        id: 7,
+        src: "assets/img/fact-1924.jpg",
+        title:"La devise olympique de Coubertin : « Citius, Altius, Fortius » (plus vite, plus haut, plus fort) est introduite à l'occasion de ces Jeux.",
+        text: "Le Baron Pierre de Coubertin se retira à l'occasion de ces Jeux Olympiques d'été de 1924. L'un des derniers souhaits du président du Comité international olympique aura été de voir son pays organiser les Jeux Olympiques après ceux contestés de 1900. Malgré l'absence du public et quelques difficultés financières, ces Jeux furent une réussite dans la participation et les performances des athlètes, mais aussi par l'intérêt grandissant pour l'événement."
+    },
+
+    {
+        id: 8,
+        src: "assets/img/fact-1928.jpg",
+        title:"Pour la première fois dans l'histoire des Jeux Olympiques, des épreuves de gymnastique et d'athlétisme sont ouvertes aux sportives féminines.",
+        text: "Le baron Pierre de Coubertin, ancien Président du Comité international olympique fut depuis toujours hostile à la participation de femmes aux Jeux Olympiques, déclarant notamment que celle-ci « constitue un affront majeur à la grandeur et à la pureté originelle de cette compétition ». L'athlète américaine Betty Robinson (deuxième en partant de la gauche) remporte la finale du 100 m femmes, le 31 juillet 1928."
+    },
+
+    {
+        id: 9,
+        src: "assets/img/fact-1932.jpg",
+        title:"Pour la première fois, la durée des jeux est de 16 jours, à la différence des éditions précédentes qui s'étalaient sur plusieurs semaines.",
+        text: "Par ailleurs, la cérémonie des médailles est désormais effectuée après les épreuves sur le lieu même de la compétition et comprend le podium du tiercé, le lever des drapeaux et l'écoute des hymnes nationaux. L'autre nouveauté fut l'utilisation officielle du chronométrage au 1/100e de seconde. Les juges en firent usage dès la finale du 100 m afin de départager les américains Eddie Tolan et Ralph Metcalfe pour 5/100e de seconde. (Photo : Le village olympique des JO de 1932)"
+    },
+
+    {
+        id: 10,
+        src: "assets/img/fact-1936.jpg",
+        title:"Plus de 4000 athlètes participent aux Jeux d'été de Berlin avec une énorme mise en scène montrant au monde entier la force du régime Nazi.",
+        text: "Les médaillés du saut en longueur saluent depuis le podium aux Jeux Olympiques d'été de Berlin, le 8 août 1936. De gauche à droite : le japonais Naoto Tajima (bronze) ; l'américain Jesse Owens (or) qui a établi un record olympique lors de l'événement et salué à la manière américaine avec la main sur le front ; et l'allemand Luz Long (argent) faisant le salut nazi sous les yeux d'Adolf Hitler."
+    },
+
+    {
+        id: 11,
+        src: "assets/img/fact-1948.jpg",
+        title:"Ce fut les premiers jeux organisés après ceux de Berlin en 1936. Les Jeux Olympiques de 1940 avaient été programmés à Tokyo puis à Helsinki mais la Seconde Guerre mondiale avait contraint à leur annulation. Ceux de 1944 prévus à Londres connurent le même sort.",
+        text: "Trois ans après la fin de la Seconde Guerre mondiale qui a fait des millions de morts, l'Europe est exsangue et en pleine période d'austérité. Au Royaume-Uni, la reconstruction de la ville de Londres, fortement touchée par les bombardements allemands (blitz) est loin d'être achevée. Aucune installation sportive n'est construite pour ces jeux. Les athlètes sont logés dans des écoles ou des baraques militaires1. Quelques équipes apportent même de la nourriture afin de pallier d'éventuels problèmes de ravitaillement."
+    },
+
+    {
+        id: 12,
+        src: "assets/img/fact-1952.jpg",
+        title:"Les Jeux Olympiques d'été de 1952 se situent en pleine Guerre froide et marquent le début de la rivalité Est-Ouest.",
+        text: "Afin de faire de ce rendez-vous olympique une fête mondiale de l'humanisme, les organisateurs finlandais ainsi que le Comité international olympique souhaitent inviter le maximum de délégations. C'est ainsi que la Russie, sous la bannière de l'URSS, fait son retour aux Jeux après quarante années d'absence. La Révolution bolcheviquede 1917 déclencha une mise à l'écart de l'Union soviétique par la communauté internationale."
+    },
+
+    {
+        id: 13,
+        src: "assets/img/fact-1956.jpg",
+        title:"En 1954, le Comité international olympique décide de scinder en deux l'organisation des jeux. Les épreuves équestres se déroulèrent par conséquent à Stockholm du 10 au 17 juin 1956.",
+        text: "Pour la première fois dans l'histoire des Jeux, la charte olympique chère à Pierre de Coubertin fut bafouée concernant l'unité de lieu. En effet, une loi australienne ignorée par le CIO lors de l'élection de Melbourne en 1949 impose une quarantaine de six mois pour tous les chevaux entrant dans le pays."
+    },
+
+    {
+        id: 14,
+        src: "assets/img/fact-1960.png",
+        title:"À 18 ans, Cassius Clay remporte l'or olympique en boxe dans la catégorie poids mi- lourds en 1960. Cinq ans plus tard, il deviendra Mohamed Ali.",
+        text: "Les médaillés olympiques en 1960 dans la catégorie poids mi-lourds sur le podium à Rome : Cassius Clay (au centre), or ; le polonais Zbigniew Pietrzykowski (à droite), argent ; Giulio Saraudi (Italie) et Anthony Madigan (Australie), médaille de bronze ex-æquo. Ces Jeux sont retransmis par plus de 100 chaînes de télévision en direct. "
+    },
+
+    {
+        id: 15,
+        src: "assets/img/fact-1964.jpg",
+        title:"Pour la première fois dans l'histoire des Jeux Olympiques, le continent asiatique accueille cet évènement.",
+        text: "Tokyo avait déjà été choisie en 1936 par le Comité international olympique pour organiser les Jeux Olympiques d'été de 1940 à l'occasion du 2 600e anniversaire de la fondation dynastique japonaise, mais la Seconde Guerre mondiale, et notamment la guerre sino-japonaise commencée dès 1937, avait forcé leur annulation. Vingt-quatre ans plus tard, les dirigeants japonais voulurent montrer que leur pays s'était relevé de la guerre et, afin de montrer leur puissance économique, ils décidèrent d'investir d'énormes capitaux dans la construction d'installations sportives ultra-modernes."
+    },
+
+    {
+        id: 16,
+        src: "assets/img/fact-1968.jpg",
+        title:"Les américains Tommie Smith (centre) et John Carlos lèvent un poing ganté de noir aux Jeux Olympiques de 1968.",
+        text: "Les sprinteurs américains Tommie Smith et John Carlos ont levé le poing au ciel et salué à la manière du Black Power aux Jeux Olympiques de Mexico en 1968. Le geste était une protestation symbolique contre le racisme aux États-Unis. Smith, le médaillé d'or, et Carlos, qui a remporté le bronze, ont été suspendus de leur équipe suite à cette action. "
+    },
+
+    {
+        id: 17,
+        src: "assets/img/fact-1972.png",
+        title:"Prise d'otages au village olympique en 1972.",
+        text: "L'organisation terroriste Septembre noir a pris en otage et tué 9 membres de l'équipe olympique d'Israël ainsi qu'un policier au village Olympique  durant les Jeux Olympiques de Munich, en 1972. "
+    },
+
+    {
+        id: 18,
+        src: "assets/img/fact-1976.jpg",
+        title:"Ces Jeux Olympiques sont marqués par le boycott de 22 nations africaines qui protestent contre la présence de la Nouvelle-Zélande.",
+        text: "Elles reprochent à cette dernière d'avoir envoyé son équipe de rugby participer à une tournée en Afrique du Sud, pays pratiquant l'apartheid. Le Comité international olympique est surpris par ce boycott. Il le conteste car l'Afrique du Sud n'est plus invitée aux Jeux depuis plus de dix ans, et que le rugby à XV n'est plus un sport olympique. Vingt-deux pays africains décident de quitter les Jeux quelques heures avant la cérémonie d'ouverture."
+    },
+
+    {
+        id: 19,
+        src: "assets/img/fact-1980.jpg",
+        title:"En décembre 1979, l'invasion de l'Afghanistan par l'Union Soviétique provoque, à l'initiative des États-Unis d'Amérique, un mouvement de protestation mondial, qui entrainera un boycott massif d'une cinquantaine de nations à ces Jeux de Moscou2.",
+        text: "En cette période de guerre froide, les Américains utilisent le boycott comme moyen de pression. Le 4 janvier 1980, le président Jimmy Carter, en pleine campagne de réélection fragilisée par la crise des otages américains en Iran, prend unilatéralement des mesures d'embargo économique contre l'URSS. Puis il demande au président du CIO de surseoir à la tenue des Jeux si les troupes soviétiques ne se retirent pas d'Afghanistan. Le 20 janvier 1980, le président américain adresse un ultimatum au Kremlin : « Si dans un mois au plus tard, vos troupes n'ont pas évacué l'Afghanistan, l'équipe olympique américaine n'ira pas à Moscou et nous demanderons aux autres pays de s'abstenir aussi »"
+    },
+
+    {
+        id: 20,
+        src: "assets/img/fact-1984.jpg",
+        title:"Après 1976 et 1980, ces Jeux Olympiques de Los Angeles furent une nouvelle fois entachés par le boycott de nations, issues cette fois-ci, des pays communistes.",
+        text: "Le 8 mai 1984, l'Union des républiques socialistes soviétiques (URSS) rend publique sa décision de ne pas envoyer de délégation aux Jeux Olympiques d'été de 1984, aux États-Unis. Elle fut suivie par une majorité de pays du bloc soviétique, dont la RDA, et Cuba. Officiellement, l'URSS refuse d'aller à Los Angeles pour des raisons de sécurité mais aussi pour l'utilisation politique que les États-Unis voudront faire de l'évènement."
+    },
+
+    {
+        id: 21,
+        src: "assets/img/fact-1988.jpg",
+        title:"Une entrée à haut risque aux Jeux Olympiques de Séoul en 1988.",
+        text: "Un cascadeur entre en parachute dans le stade olympique durant la cérémonie d'ouverture. 7 pays boycottent ces Jeux Olympiques en faveur de la Corée du Nord."
+    },
+
+    {
+        id: 22,
+        src: "assets/img/fact-1992.jpg",
+        title:"Le père du britannique Derek Redmond l'aide à franchir la ligne d'arrivée en 1992.",
+        text: "Le britannique Derek Redmond grimace de douleur tandis que son père l'aide à franchir la ligne d'arrivée du stade olympique de Barcelone, le 3 août 1992. Foudroyé par un claquage lors de son 400 m, l'athlète a tenu à terminer la course, sous les applaudissements du public. "
+    },
+
+    {
+        id: 23,
+        src: "assets/img/fact-1996.jpg",
+        title:"Ces Jeux furent aussi marqués par l'explosion, au beau milieu de la foule, d'une bombe dans le parc du Centenaire à Atlanta, faisant 2 morts et 112 blessés.",
+        text: "Des polémiques furent également engagées sur la légitimité du choix de la ville d'Atlanta au détriment d'Athènes pour les Jeux du centenaire, et sur les problèmes d'organisation liés aux transports et à l'informatique."
+    },
+
+    {
+        id: 24,
+        src: "assets/img/fact-2000.jpg",
+        title:"La Corée du Sud et la Corée du Nord marchent sous un seul drapeau, en 2000.",
+        text: "Les porteurs de drapeau Eun-Soon Chung et Jang-Choo Pak mènent l'équipe olympique coréenne, issue du Nord et du Sud, dans un geste de réconciliation, durant la cérémonie d'ouverture des Jeux Olympiques de Sydney en 2000. "
+    },
+
+    {
+        id: 25,
+        src: "assets/img/fact-2004.jpg",
+        title: "Le Comité international olympique confie l'organisation des Jeux Olympiques d'été de 2004 à la ville d'Athènes au cours de la 106e session du 5 septembre 1997 à Lausanne.",
+        text: "Le succès d'Athènes est en grande partie fondé sur la volonté des organisateurs de faire appel à l'histoire et au rôle qu'ont joué la Grèce dans la promotion du mouvement olympique. Cette attribution est perçue pour beaucoup comme une compensation à l'échec de la capitale grecque à l'organisation des Jeux du centenaire de 1996 qui aurait dû revenir de droit à Athènes, berceau de l'olympisme, même si à cause des Jeux de 1992 à Barcelone, l'alternance des continents n'aurait alors pas été respectée."
+    },
+
+    {
+        id: 26,
+        src: "assets/img/fact-2008.jpg",
+        title:"Pour la première fois, le comité d'organisation a mis en place un système de diffusion des Jeux sur certaines plates-formes de partage.",
+        text: "Un accord a été signé entre le CIO et YouTube afin de diffuser des images, parfois en exclusivité, de certaines compétitions dans 77 pays qui ne retransmettent pas les Jeux. Ce partenariat a également permis de lutter contre la diffusion, sur ces mêmes plates-formes, d'images piratées et obtenues sans accord. Avant et durant les Jeux, l'accès libre à Internet depuis le sol chinois a représenté un sujet de controverse pour de nombreux journalistes. Le coureur jamaïcain Usain Bolt réussit le doublé 100 m et 200 m en battant à chaque fois le record du monde."
+    },
+
+    {
+        id: 27,
+        src: "assets/img/fact-2012.jpg",
+        title:"La pollution inquiète beaucoup les organisateurs des Jeux de Londres, car celle-ci pourrait nuire aux performances des sportifs eux-mêmes.",
+        text: "« Plusieurs études ont montré que l'exposition à l'ozone ou aux microparticules quand on fait du sport réduit la capacité pulmonaire, diminue les performances et surtout augmente le risque d'asthme» explique le Dr Pierre Souvet, président de l'Association santé environnement France. En 1984, aux Jeux de Los Angeles, le coureur de 800 mètres Steve Ovett s'était effondré pendant la finale en raison de problèmes respiratoires liés en partie selon lui à la pollution. En 2008 aux Jeux de Pékin, les autorités avaient réduit la circulation des voitures et fermé des centaines d'usines. À Londres, on craint d'être confronté au même problème."
+    },
+    {
+        id: 28,
+        src: "assets/img/fact-2016.jpg",
+        title: "Le nageur américain Michael Phelps est à nouveau l'athlète le plus médaillé de ces Jeux.",
+        text: "Il remporte cinq titres olympiques et une médaille d'argent. Il porte à Rio son record d'olympien le plus médaillé de tous les temps à 23 titres olympiques (dont 13 titres individuels) et 28 médailles en tout."
+    }
+
+]
+
+
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'YOURPASSWORD',
+    database: 'olympics'
+})
+
+
+facts.forEach(fact => {
+    let query = `
+        INSERT INTO 
+            fact 
+            (title, text, src, game_id)
+        VALUES 
+            ("${fact.title}", "${fact.text}", "${fact.src}", ${fact.id})`
+    
+    connection.query(query, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    })
+})
