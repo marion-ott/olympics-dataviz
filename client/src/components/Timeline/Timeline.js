9b657576c79cd0a8401cc666287d5307dfbfbfef
Manon Ragnotti <m.ragnotti@mazarinedigital.com>
2019-06-06 16:18:38 +0200

    Style Timeline

--- client/src/components/Timeline/Timeline.js
+++ client/src/components/Timeline/Timeline.js
@@ -2,13 +2,57 @@ import React from 'react'
 import './styles.scss'
 
 class Timeline extends React.Component {
+
+    addClass = (index) => {
+
+        const allDates = Array.from(document.querySelectorAll('.Timeline ul li'));
+        const elementClick = index.target;
+
+        allDates.forEach((element, index) => {
+            if (element  === elementClick) {
+                element.classList.add('active');
+            }
+            else {
+                element.classList.remove('active');
+            }
+        })
+
+        // elementClick.classList.add('active');
+        // allDates.forEach((element, index) => {
+
+        // const allItems = Array.from(document.querySelectorAll('a'));
+        // allItems.forEach((element, index) => {
+        //     if (element.className === 'active') {
+        //         element.classList.remove('active');
+        //     }
+        // })
+        //
+        // //Récupère l'element click
+        // const element = index.target;
+        // //Retourne l'index de l'element click
+        // const indexElement = parseInt(element.getAttribute('index'));
+        //
+        // //Selection et affichage des div contenu
+        // const divContent = Array.from(document.querySelectorAll('.container div'));
+        // divContent.forEach((el, index) => {
+        //     if (index === indexElement) {
+        //         el.classList.add('activeContent');
+        //         element.classList.add('active');
+        //
+        //     } else {
+        //         el.classList.remove('activeContent');
+        //     }
+        // })
+
+    }
+
     render() {
         return(
             <section className="Timeline">
                 <ul>
                     {
                         this.props.games.map((game, key) => (
-                            <li key={key} id={game.id} onClick={(event) => this.props.updateGame(event)}>{game.year}</li>
+                            <li key={key} id={game.id} onClick={(event) => {this.props.updateGame(event); this.addClass(event)}}>{game.year}</li>
                         ))
                     }
                 </ul>
